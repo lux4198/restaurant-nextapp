f799cf13b92e94215e59df5a88a598632074c4d2
Lukas <lukas.nittka@posteo.de>
2022-09-09 13:21:46 +0200

    transcribed design system to global css

--- code/pages/_document.js
+++ code/pages/_document.js
@@ -5,10 +5,8 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link
-          link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@300;600;700&display=swap" 
-          rel="stylesheet"
-        />
+      <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@300;400;600;700&display=swap" 
+      rel="stylesheet"/>
       </Head>
       <body>
         <Main />
--- code/pages/index.js
+++ code/pages/index.js
@@ -2,7 +2,12 @@
 export default function Home() {
   return (
     <div >
-      <h1>hello there</h1>
+      <h1>Jived Fox</h1>
+      <h2>Jived Fox</h2>
+      <h3>Jived Fox</h3>
+      <h4>Family Gathering</h4>
+
+      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. </p>
     </div>
   )
 }
