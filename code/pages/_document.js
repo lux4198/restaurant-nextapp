37d53aa9e46d152749538a4bdce880cccca3a70f
Lukas <lukas.nittka@posteo.de>
2022-09-09 12:58:17 +0200

    initialized next project

---
+++ code/pages/_document.js
@@ -0,0 +1,19 @@
+
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@300;600;700&display=swap" 
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
